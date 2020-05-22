04912f7ba0fbca3b6f08f137fc709838af8dd220
Oscar Sequeiros <oscar.sequeiros_ex@scotibank.cl>
2020-05-22 00:10:22 -0400

    Adding queue

--- src/App.kt
+++ src/App.kt
@@ -1,6 +1,15 @@
+import doublylinkedlist.DoublyLinkedList
+import queue.ArrayListQueue
+import queue.LinkedListQueue
+
 fun main() {
 
     val aString = "((()))"
     println(aString.checkParentheses())
 
+    val myQueue = LinkedListQueue<String>().apply {
+        enqueue("Tom")
+        enqueue("Rick")
+        enqueue("Harry")
+    }
 }
---
+++ src/queue/ArrayListQueue.kt
@@ -0,0 +1,24 @@
+package queue
+
+class ArrayListQueue<T> : Queue<T> {
+
+    private val list = arrayListOf<T>()
+
+    override fun enqueue(element: T): Boolean {
+        list.add(element)
+        return true
+    }
+
+    override fun dequeue(): T? {
+        return if (isEmpty) null else list.removeAt(0)
+    }
+
+    override val count: Int
+        get() = list.size
+
+    override fun peek(): T? {
+        return list.firstOrNull()
+    }
+
+    override fun toString(): String = list.toString()
+}
