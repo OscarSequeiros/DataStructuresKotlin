a1bd203c1521143c12dad6af78277f658631bc7b
Oscar Sequeiros <oscar.sequeiros_ex@scotibank.cl>
2020-05-21 23:20:34 -0400

    Adding DoublyLinkedList.

---
+++ src/doublylinkedlist/Node.kt
@@ -0,0 +1,14 @@
+package doublylinkedlist
+
+class Node<T>(var value: T,
+              var next: Node<T>? = null,
+              var previous: Node<T>? = null) {
+
+    override fun toString(): String {
+        return if (next != null) {
+            "$value -> ${next.toString()}"
+        } else {
+            "$value"
+        }
+    }
+}
